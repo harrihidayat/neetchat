ef2d6efbc1737cfb4e458828712c14fcda70ec39
Harri <harri.hidayat95@gmail.com>
2016-12-22 01:17:10 +0700

    3

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app  = express();
 var http = require('http').Server(app);
 var io   = require('socket.io')(http);
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
@@ -57,6 +59,6 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
-	console.log('run on 3000');
+http.listen(app.get('port'), function(){
+	console.log('Node app is running on port', app.get('port'));
 });
