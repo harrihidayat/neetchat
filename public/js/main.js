f68ad6ea74dbca19d22ed0e3edcdd8b449160ef1
Harri <harri.hidayat95@gmail.com>
2016-12-22 00:34:48 +0700

    commit awal

---
+++ public/js/main.js
@@ -0,0 +1,104 @@
+var socket = io();
+var username = '';
+
+$('#kirim').on('click', function(){
+	socket.emit('newMessage', username + ': ' +$('#text_box').val());
+	$('#text_box').val('');
+	isTyping = false;
+	return false;
+});
+
+$('#text_box').bind("enterKey",function(e){
+   socket.emit('newMessage', username + ': ' +$('#text_box').val());
+	$('#text_box').val('');
+	isTyping = false;
+	return false;
+});
+
+
+//daftar user
+socket.on('addOnlineUsers', function(usernames){
+	$('#user_lists').empty();
+
+	for(var i=0; i<usernames.length; i++){
+		$('#user_lists').append($('<li class="media" id="user_' + usernames[i] + '">')
+						.append($('<div class="media-body">')
+							.append($('<div class="media">')
+								.append($('<img class="media-object img-circle pull-left" style="max-height:40px;" src="img/user.png" />'))
+								.append($('<h5>').text(usernames[i])
+								.append($('<h5><small class="text-muted"> Online Sejak'+ $.format.date(new Date(), ' d-MMMM-yyyy') +'</small>'))))));			
+	}
+});
+
+//untuk nampilin di frontend
+socket.on('newMessage', function(msg){
+	$('#messages').append($('<li class="media">')
+				  .append($('<div class="media-body">')
+				  .append($('<img class="pull-left" class="media-object img-circle " src="img/user.png" />'))
+				  .append($('<div class="media-body" >')
+				  .text(msg)
+				  .append($('<br /><br />'))
+				  .append($('<small class="text-muted">'+ $.format.date(new Date(), ' d-MMMM-yyyy ~ h:mm:ss - a') +'</small>')
+				  .append($('<hr />'))))));
+	$('.tempchat').remove();
+});
+
+//ketika user daftar nama -> masuk ke chatroom
+$('#submit_name').click(function(){
+	//validasi apakah nama sudah ada apa belum
+	if($('#nama_user') != ''){
+		//menguji nama exist atau belum
+		username = $('#nama_user').val();
+		socket.emit('registerUser', username);
+	} 
+});
+
+$('#nama_user').keyup(function(e){
+    if(e.keyCode == 13)
+    {
+        $(this).trigger("masuk");
+    }
+});
+
+$('#nama_user').bind("masuk",function(e){
+   //validasi apakah nama sudah ada apa belum
+	if($('#nama_user') != ''){
+		//menguji nama exist atau belum
+		username = $('#nama_user').val();
+		socket.emit('registerUser', username);
+	} 
+});
+
+
+
+//menerima respon dari server saat register
+socket.on('registerRespond', function(status) {
+	if (status == false) {
+		swal(
+		  'Wadoh...',
+		  'Nama itu udah kepake cok, ganti aja!',
+		  'error'
+		)
+	} else {
+		$('#chatroom').removeClass('hidden');
+		$('#homepage').addClass('hidden');
+	}
+});
+
+//ketika user mengetik
+var isTyping = false;
+$('#text_box').keyup(function(e){
+	if(isTyping == false) {
+		socket.emit('newTyping', username + ' sedang mengetik..');
+	}
+	isTyping = true;
+
+	if(e.keyCode == 13)
+    {
+        $(this).trigger("enterKey");
+    }
+});
+
+socket.on('newTyping', function(msg){
+	$('#messages').append($('<li class="tempchat">').text(msg));
+});
---
+++ server.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var app  = express();
+var http = require('http').Server(app);
+var io   = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');	
+});
+
+var users 	  = {};
+var usernames = [];
+
+io.on('connection', function(socket){
+
+	
+
+	//ketika ada user yang daftar
+	socket.on('registerUser', function(username){
+		if(usernames.indexOf(username) != -1){
+			socket.emit('registerRespond', false);
+		} else {
+			users[socket.id] = username;
+			//respon ketika ada  user
+			socket.broadcast.emit('newMessage', 'BROADCAST: ada orang baru masuk!');
+			usernames.push(username);
+			socket.emit('registerRespond', true);
+			io.emit('addOnlineUsers', usernames);
+
+			
+		}
+	});
+
+	//kalo ada message baru
+	socket.on('newMessage', function(msg){
+		io.emit('newMessage', msg);
+		console.log('chat baru: ' + msg);
+	});
+
+	//kalo ada yang mengetik pesan
+	socket.on('newTyping', function(msg){
+		io.emit('newTyping', msg);
+	});
+
+	//kalo ada user disconnect
+	socket.on('disconnect', function(msg){
+		socket.broadcast.emit('newMessage', 'BROADCAST: ada orang yang keluar!');
+
+		var index = usernames.indexOf(users[socket.id]);
+		usernames.splice(index, 1);
+
+		delete users[socket.id];
+
+		io.emit('addOnlineUsers', usernames);
+	});
+
+});
+
+http.listen(3000, function(){
+	console.log('run on 3000');
+});
